d4e37b93dbcaa4e5351c0e824785a5c1dc77b699
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2021-05-09 23:57:50 +0545

    mern-simplesetup completed

---
+++ client/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>Hi, I am <br />Dipendra Bahadur Chand !</h1>
+        </div>
+    )
+}
+
+export default hot(module)(HelloWorld)
